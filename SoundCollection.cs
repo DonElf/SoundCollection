cc1338b2ade80f9f30a2cbd73f065003f783d0cc
Don_Elf <46205043+DonElf@users.noreply.github.com>
2025-12-04 11:50:24 +1100

    Added boilerplate/header to script.

--- SoundCollection.cs
+++ SoundCollection.cs
@@ -1,3 +1,21 @@
+/* ------------------------------------
+ |
+ |   A scriptable object to hold and play audio clips
+ |   By Don_Elf
+ |   https://github.com/DonElf/SoundCollection
+ |
+ |   ©2025 Don_Elf
+ |   Some Rights Reserved.
+ |
+ |
+ |   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ |   http://www.apache.org/licenses/LICENSE-2.0
+ |
+ |   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ |   See the License for the specific language governing permissions and limitations under the License.
+ | 
+------------------------------------ */
+
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
@@ -227,4 +245,5 @@ namespace SC {
         public static implicit operator int(CheapHash32<T> hash32) { return hash32.hash; }
     }
 #endif
+
 }
